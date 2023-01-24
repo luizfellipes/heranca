33f840da6c6722e1f29844c7fa21ff408a0fb755
luizfellipes <luiz.13.silva@gmail.com>
2023-01-24 16:56:12 -0400

    Criado as classes Pessoa, professor, aluno e funcionado

---
+++ src/main/java/org/example/Professor.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Professor extends Pessoa{
+    private String especialidade;
+    private float salario;
+
+    public Professor(String especialidade, float salario) {
+        this.especialidade = especialidade;
+        this.salario = salario;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public void receberAumento(){
+
+    }
+}
